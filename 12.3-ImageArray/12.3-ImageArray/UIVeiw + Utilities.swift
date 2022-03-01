c3c8c51f70fa200e5d377487d90641e766ae671f
atrapashka <a.trapashka@gmail.com>
2022-03-01 10:00:14 +0300

    add 12.3

---
+++ 12.3-ImageArray/12.3-ImageArray/UIVeiw + Utilities.swift
@@ -0,0 +1,15 @@
+//
+//  UIVeiw + Utilities.swift
+//  12.3-ImageArray
+//
+//  Created by Alehandro on 21.02.22.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+    func applyImageUtilities() {
+        self.contentMode = .scaleAspectFit
+    }
+}
